4567e6f4607b65ee80d11e2a19347c3cda242e94
Mohammad Kanaan <mkanaan@ejada.com>
2025-11-05 14:46:43 +0300

    added core concepts list to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { CORE_CONCEPTS } from "./data.js";
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core', 'exquisite', 'fascinating'];
 
 function genRandomInt(max) {
@@ -15,12 +17,48 @@ function Header() {
     </header>
   );
 }
+
+function CoreConcepts(props){
+  return(
+    <li>
+      <img src={props.image} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+
+}
 function App() {
   return (
     <div>
       <Header />
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>core concepts</h2>
+          <ul>
+            <CoreConcepts 
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} 
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image} 
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image} 
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image} 
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
